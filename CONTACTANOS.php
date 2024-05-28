c7eeba374f765c252d25df242cd79cd9e0b864aa
JaureguiCoraiteAlexisEstaban <153701015+JaureguiCoraiteAlexisEstabn@users.noreply.github.com>
2024-05-27 23:14:57 -0400

    Update CONTACTANOS.php

--- CONTACTANOS.php
+++ CONTACTANOS.php
@@ -6,7 +6,7 @@
 			<div class="content-banner">
 				<p>Contactanos</p>
 				<h2>Para contactarnos por favor haga clic en el enlace! <br /></h2>
-				<a href="#" onclick="openPopup()">enlace</a>
+				<a href="#" onclick="openPopup6()">enlace</a>
 			</div>
 		</section>
 
@@ -18,7 +18,7 @@
 		></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script>
-        function openPopup() {
+        function openPopup6() {
         var url = 'contacta.php'
         var opciones = 'width=400,height=400,scrollbars=yes,resizable=yes';
         window.open(url, '_blank', opciones);
