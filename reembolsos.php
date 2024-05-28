2f83cea17bd8a7b022e738e7dd23b80068857f35
JaureguiCoraiteAlexisEstaban <153701015+JaureguiCoraiteAlexisEstabn@users.noreply.github.com>
2024-05-27 23:01:49 -0400

    Add files via upload

---
+++ reembolsos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reembolsos</title>
+    <link rel="stylesheet" href="cssc/bootstrap.min.css" />
+    <link rel="stylesheet" href="cssc/font-awesome.min.css" />
+    <link rel="stylesheet" href="cssc/contact.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,700,900,900i">
+</head>
+<body>
+    <h1 class="titulo-contacto text-center">REEMBOLSOS</h1>
+    <h3>1. Libros defectuosos o dañados:</h3>
+    <P>Si un cliente recibe un libro en mal estado o defectuoso, el espacio de literatura puede ofrecer reembolsos para garantizar la satisfacción del cliente.</P>
+    <h3>2. Cancelaciones de pedidos:</h3>
+    <p>Si un cliente desea cancelar un pedido antes de que sea enviado, el espacio de literatura puede ofrecer reembolsos completos o parciales según su política de cancelación.</p>
+    <h3>3. Devoluciones por insatisfacción:</h3>
+    <p>Si un cliente no está satisfecho con un libro que ha comprado, es posible que desee devolverlo y recibir un reembolso. El espacio de literatura podría ofrecer reembolsos en estos casos, según su política de devolución.</p>
+    <h3>4. Errores en el procesamiento de pedidos:</h3>
+    <p>Si se comete un error durante el procesamiento de un pedido, como enviar el libro incorrecto o enviar la cantidad incorrecta de libros, el espacio de literatura podría ofrecer reembolsos para rectificar el error.</p>
+    <h3>5. Cambios en las condiciones de venta:</h3>
+    <p>Si el espacio de literatura cambia las condiciones de venta después de que un cliente haya realizado una compra, como cambios en el precio o en las condiciones de envío, podría ofrecer reembolsos a los clientes que ya han realizado compras bajo las condiciones anteriores.</p>
+    <script src="jsc/jquery-1.12.4.min.js"></script>
+    <script src="jsc/bootstrap.min.js"></script>
+    <script src="jsc/custom.js"></script>
+</body>
+</html>
---
+++ terminos.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terminos y Condiciones</title>
+    <link rel="stylesheet" href="cssc/bootstrap.min.css" />
+    <link rel="stylesheet" href="cssc/font-awesome.min.css" />
+    <link rel="stylesheet" href="cssc/contact.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,700,900,900i">
+</head>
+<body>
+    <center><h1 class="titulo-contacto text-center">TERMINOS Y CONDICIONES</h1></center>
+    <h3>1. Propiedad Intelectual:</h3>
+    <p>- Todo el contenido publicado en este espacio, incluyendo pero no limitado a textos, imágenes, y cualquier otra forma de expresión artística, es propiedad intelectual de sus respectivos autores.</p>
+    <p>- Al publicar contenido en este espacio, otorgas una licencia no exclusiva a la administración del espacio para mostrar, distribuir y promocionar tu obra dentro del marco de este espacio de literatura.</p>
+    <h3>2. Responsabilidad del Contenido:</h3>
+    <p>- Los autores son los únicos responsables del contenido que publican en este espacio. La administración no se hace responsable por el contenido generado por los usuarios.</p>
+    <p>- No se permite la publicación de material que sea ilegal, difamatorio, obsceno, ofensivo, o que viole los derechos de propiedad intelectual de terceros.</p>
+    <h3>3. Comportamiento del Usuario:</h3>
+    <p>- Los usuarios deben comportarse de manera respetuosa y cortés hacia otros miembros de la comunidad.</p>
+    <p>- No se tolerará el acoso, la intimidación, o cualquier forma de comportamiento abusivo hacia otros usuarios.</p>
+    <h3>4. Privacidad:</h3>
+    <p>- La administración se compromete a proteger la privacidad de los usuarios y no compartirá su información personal con terceros sin su consentimiento, a menos que así lo requiera la ley.</p>
+    <p>- Los usuarios son responsables de proteger su propia privacidad y no deben compartir información personal sensible en este espacio.</p>
+    <h3>5. Modificación de Términos y Condiciones:</h3>
+    <p>- La administración se reserva el derecho de modificar estos términos y condiciones en cualquier momento sin previo aviso.</p>
+    <p>- Los cambios entrarán en vigor inmediatamente después de su publicación en este espacio.</p>
+    <br>
+    <p>Última actualización: 26/05/2024</p>
+    <script src="jsc/jquery-1.12.4.min.js"></script>
+    <script src="jsc/bootstrap.min.js"></script>
+    <script src="jsc/custom.js"></script>
+</body>
+</html>
