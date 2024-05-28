01c39bfd926b2b0a74da6638bc4be6f89eafc6d7
JaureguiCoraiteAlexisEstaban <153701015+JaureguiCoraiteAlexisEstabn@users.noreply.github.com>
2024-05-27 23:00:54 -0400

    Add files via upload

---
+++ delivery.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delivery</title>
+    <link rel="stylesheet" href="cssc/bootstrap.min.css" />
+    <link rel="stylesheet" href="cssc/font-awesome.min.css" />
+    <link rel="stylesheet" href="cssc/contact.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,700,900,900i">
+</head>
+<body>
+    <center><h1 class="titulo-contacto text-center">DELIVERY</h1></center>
+    <h3>1. Entrega de libros:</h3>
+    <p>Algunas librerías o servicios de entrega de libros a domicilio pueden ofrecer un servicio de "delivery" donde los clientes pueden pedir libros y recibirlos en la comodidad de su hogar.</p>
+    <h3>2. Entrega de comida o bebida durante eventos literarios:</h3>
+    <p>En eventos literarios, conferencias o clubes de lectura, a veces se ofrece servicio de comida y bebida para los asistentes. En este caso, el término "delivery" podría referirse a la entrega de comida y bebida durante el evento.</p>
+    <h3>3. Entrega de literatura a través de medios digitales:</h3>
+    <p>En la era digital, muchas obras literarias están disponibles en formato electrónico. El término "delivery" podría usarse para describir la entrega o distribución de libros electrónicos a través de plataformas en línea o aplicaciones de lectura.</p>
+    <h3>4. Entrega de contenido literario personalizado:</h3>
+    <p>Algunas empresas ofrecen servicios de entrega de contenido literario personalizado, como suscripciones a cajas de libros temáticos o clubes de lectura que envían libros seleccionados especialmente a los suscriptores en intervalos regulares.</p>
+    <script src="jsc/jquery-1.12.4.min.js"></script>
+    <script src="jsc/bootstrap.min.js"></script>
+    <script src="jsc/custom.js"></script>
+</body>
+</html>
