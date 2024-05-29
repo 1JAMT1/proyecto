71eca018ffae3da9891d369d8a85587f6f655d65
JaureguiCoraiteAlexisEstaban <153701015+JaureguiCoraiteAlexisEstabn@users.noreply.github.com>
2024-05-29 10:47:27 -0400

    Update privacidad.php

--- privacidad.php
+++ privacidad.php
@@ -1,34 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Politicas de Privacidad</title>
-    <link rel="stylesheet" href="cssc/bootstrap.min.css" />
-    <link rel="stylesheet" href="cssc/font-awesome.min.css" />
-    <link rel="stylesheet" href="cssc/contact.css" />
+<?php
+    $inicio=false;
+    include "../espaciodeliteratura/includes/templates/header.php";
+?>
+    <link rel="stylesheet" href="../../cssa/bootstrap.min.css">
+    <link rel="stylesheet" href="../../cssc/font-awesome.min.css" />
+    <link rel="stylesheet" href="../../cssc/contact.css" />
+    <link rel="stylesheet" href="estilo.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,700,900,900i">
-</head>
-<body>
-    <h1 class="titulo-contacto text-center">POLITICAS DE PRIVACIDAD</h1>
-    <h3>1. Información Recopilada:</h3>
-    <p>- Información de Registro: Al registrarte en este espacio de literatura, recopilamos tu nombre de usuario, dirección de correo electrónico y cualquier otra información que nos proporciones voluntariamente.</p>
-    <p>- Información de Uso: Recopilamos información sobre cómo interactúas con este espacio, como tus actividades de navegación, la frecuencia de visitas y las páginas que visitas.</p>
-    <h3>2. Uso de la Información:</h3>
-    <p>- Utilizamos la información recopilada para proporcionar y mejorar los servicios de este espacio de literatura, para comunicarnos contigo sobre tu cuenta y para personalizar tu experiencia de usuario.</p>
-    <p>- No compartiremos tu información personal con terceros, excepto cuando sea necesario para proporcionar los servicios solicitados por ti, cumplir con la ley o proteger nuestros derechos.</p>
-    <h3>3. Cookies y Tecnologías Similares:</h3>
-    <p>- Utilizamos cookies y otras tecnologías de seguimiento para recopilar información sobre tus actividades en este espacio y para mejorar la funcionalidad y la seguridad del sitio.</p>
-    <p>- Puedes controlar el uso de cookies a través de la configuración de tu navegador. Sin embargo, desactivar las cookies puede afectar tu experiencia de usuario en este espacio.</p>
-    <h3>4. Seguridad de la Información:</h3>
-    <p>- Tomamos medidas de seguridad razonables para proteger la información personal de los usuarios contra el acceso no autorizado, la divulgación o la alteración.</p>
-    <p>- Sin embargo, ningún método de transmisión por Internet o almacenamiento electrónico es 100% seguro, por lo que no podemos garantizar la seguridad absoluta de la información transmitida a través de este espacio.</p>
-    <h3>5. Menores de Edad:</h3>
-    <p>- Este espacio de literatura no está dirigido a menores de 13 años y no recopilamos intencionalmente información personal de niños menores de esa edad sin el consentimiento verificable de sus padres o tutores.</p>
-    <h3>6. Cambios en la Política de Privacidad:</h3>
-    <p>- Nos reservamos el derecho de actualizar esta Política de Privacidad en cualquier momento. Te notificaremos sobre cualquier cambio significativo en la forma en que tratamos tu información personal enviándote un aviso a la dirección de correo electrónico proporcionada o mediante un aviso destacado en este espacio de literatura.</p>
-    <script src="jsc/jquery-1.12.4.min.js"></script>
-    <script src="jsc/bootstrap.min.js"></script>
-    <script src="jsc/custom.js"></script>
-</body>
-</html>
+    <br>
+    <center><h1 class="titulo">POLITICAS DE PRIVACIDAD</h1></center>
+    <h2>1. Información Recopilada:</h2>
+    <h5>- Información de Registro: Al registrarte en este espacio de literatura, recopilamos tu nombre de usuario, dirección de correo electrónico y cualquier otra información que nos proporciones voluntariamente.</h5>
+    <h5>- Información de Uso: Recopilamos información sobre cómo interactúas con este espacio, como tus actividades de navegación, la frecuencia de visitas y las páginas que visitas.</h5>
+    <h2>2. Uso de la Información:</h2>
+    <h5>- Utilizamos la información recopilada para proporcionar y mejorar los servicios de este espacio de literatura, para comunicarnos contigo sobre tu cuenta y para personalizar tu experiencia de usuario.</h5>
+    <h5>- No compartiremos tu información personal con terceros, excepto cuando sea necesario para proporcionar los servicios solicitados por ti, cumplir con la ley o proteger nuestros derechos.</h5>
+    <h2>3. Cookies y Tecnologías Similares:</h2>
+    <h5>- Utilizamos cookies y otras tecnologías de seguimiento para recopilar información sobre tus actividades en este espacio y para mejorar la funcionalidad y la seguridad del sitio.</h5>
+    <h5>- Puedes controlar el uso de cookies a través de la configuración de tu navegador. Sin embargo, desactivar las cookies puede afectar tu experiencia de usuario en este espacio.</h5>
+    <h2>4. Seguridad de la Información:</h2>
+    <h5>- Tomamos medidas de seguridad razonables para proteger la información personal de los usuarios contra el acceso no autorizado, la divulgación o la alteración.</h5>
+    <h5>- Sin embargo, ningún método de transmisión por Internet o almacenamiento electrónico es 100% seguro, por lo que no podemos garantizar la seguridad absoluta de la información transmitida a través de este espacio.</h5>
+    <h2>5. Menores de Edad:</h2>
+    <h5>- Este espacio de literatura no está dirigido a menores de 13 años y no recopilamos intencionalmente información personal de niños menores de esa edad sin el consentimiento verificable de sus padres o tutores.</h5>
+    <h2>6. Cambios en la Política de Privacidad:</h2>
+    <h5>- Nos reservamos el derecho de actualizar esta Política de Privacidad en cualquier momento. Te notificaremos sobre cualquier cambio significativo en la forma en que tratamos tu información personal enviándote un aviso a la dirección de correo electrónico proporcionada o mediante un aviso destacado en este espacio de literatura.</h5>
+    <br>
+<?php 
+	include "../espaciodeliteratura/includes/templates/footer.php"; 
+?>
+    <script src="../../jsa/bootstrap.min.js" ></script>
+<script
+	src="https://kit.fontawesome.com/81581fb069.js"
+></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
