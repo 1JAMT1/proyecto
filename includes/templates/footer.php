fa5070dd63a780fa40b6812016c45efec4c25963
JaureguiCoraiteAlexisEstaban <153701015+JaureguiCoraiteAlexisEstabn@users.noreply.github.com>
2024-05-29 10:49:47 -0400

    Update footer.php

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/espaciodeliteratura/css/gotop.css">
-    <link rel="stylesheet" href="/espaciodeliteratura/styles.css">
-    <link rel="stylesheet" href="/espaciodeliteratura/estilos.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="/espaciodeliteratura/css/gotop.css">
+	<link rel="stylesheet" href="/espaciodeliteratura/styles.css">
+	<link rel="stylesheet" href="/espaciodeliteratura/estilos.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="go-top-container">
-	<div class="go-top-button">
-		<i class="fas fa-chevron-up"></i>
+	<div class="go-top-container">
+		<div class="go-top-button">
+			<i class="fas fa-chevron-up"></i>
+		</div>
 	</div>
-    </div>
     <footer class="footer">
 			<div class="container container-footer">
 				<div class="menu-footer">
@@ -22,7 +22,7 @@
 						<p class="title-footer">Información de Contacto</p>
 						<ul>
 							<li>
-							    Dirección: 777 LA paz bo, 
+								Dirección: 777 LA paz bo, 
 							</li>
 							<li>Teléfono: 1234567890</li>
 							<li>Fax: 4444444444</li>
@@ -51,9 +51,9 @@
 						<p class="title-footer">Información</p>
 						<ul>
 							<li><a href="/espaciodeliteratura/QUIENES SOMOS.php">Acerca de Nosotros</a></li>
-							<li><a href="#" onclick="openPopup1()">Información Delivery</a></li>
-							<li><a href="#" onclick="openPopup2()">Politicas de Privacidad</a></li>
-							<li><a href="#" onclick="openPopup3()">Términos y condiciones</a></li>
+							<li><a href="/espaciodeliteratura/delivery.php">Información Delivery</a></li>
+							<li><a href="/espaciodeliteratura/privacidad.php">Politicas de Privacidad</a></li>
+							<li><a href="/espaciodeliteratura/terminos.php">Términos y condiciones</a></li>
 							<li><a href="/espaciodeliteratura/CONTACTANOS.php">Contactános</a></li>
 						</ul>
 					</div>
@@ -64,8 +64,8 @@
 						<ul>
 							<li><a href="/espaciodeliteratura/admin/usuario/index.php">Mi cuenta</a></li>
 							<li><a href="/espaciodeliteratura/admin/pedido/listado.php">Historial de ordenes</a></li>
-							<li><a href="#" onclick="openPopup5()">Boletín</a></li>
-							<li><a href="#" onclick="openPopup6()">Reembolsos</a></li>
+							<li><a href="/espaciodeliteratura/boletin.php">Boletín</a></li>
+							<li><a href="/espaciodeliteratura/reembolsos.php">Reembolsos</a></li>
 						</ul>
 					</div>
 
@@ -78,7 +78,7 @@
 								día con nuevas colecciones y ofertas exclusivas.
 							</p>
 							<input type="email" placeholder="Ingresa el correo aquí...">
-							<button>Suscríbete</button>
+							<button onclick="openPopup1()">Suscríbete</button>
 						</div>
 					</div>
 				</div>
@@ -88,56 +88,29 @@
 						Desarrollado por G1
 					</p>
 
-					<img src="/espaciodeliteratura/img/payment.png" href="#" onclick="openPopup9()" alt="Pagos">
+					<img src="/espaciodeliteratura/img/payment.png" onclick="openPopup9()">
 				</div>
 			</div>
-	    		<script src="/espaciodeliteratura/js/gotop.js"></script>
+			<script src="/espaciodeliteratura/js/gotop.js"></script>
 			<script>
         		function openPopup1() {
-        		var url = 'delivery.php'
+        		var url = 'suscribete.php'
         		var opciones = 'width=400,height=400,scrollbars=yes,resizable=yes';
         		window.open(url, '_blank', opciones);
         		}
 			</script>
 			<script>
-        		function openPopup2() {
-        		var url = 'privacidad.php'
-        		var opciones = 'width=400,height=400,scrollbars=yes,resizable=yes';
-        		window.open(url, '_blank', opciones);
-        		}
-			</script>
-			<script>
-        		function openPopup3() {
-        		var url = 'terminos.php'
-        		var opciones = 'width=400,height=400,scrollbars=yes,resizable=yes';
-        		window.open(url, '_blank', opciones);
-        		}
-			</script>
-			<script>
-        		function openPopup5() {
-        		var url = 'boletin.php'
-        		var opciones = 'width=400,height=400,scrollbars=yes,resizable=yes';
-        		window.open(url, '_blank', opciones);
-        		}
-			</script>
-			<script>
-        		function openPopup6() {
-        		var url = 'reembolsos.php'
-        		var opciones = 'width=400,height=400,scrollbars=yes,resizable=yes';
-        		window.open(url, '_blank', opciones);
-        		}
-			</script>
-	    		<script>
         		function openPopup9() {
         		var url = 'pagos.php'
         		var opciones = 'width=400,height=400,scrollbars=yes,resizable=yes';
         		window.open(url, '_blank', opciones);
         		}
 			</script>
-	    		<script
+			<script
 			src="https://kit.fontawesome.com/81581fb069.js"
 			></script>
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 	</footer>
 </body>
 </html>
