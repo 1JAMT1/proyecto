f1041df690507b39009f3417ee7be92d2637880d
1JAMT1 <jamtcamamani@gmail.com>
2024-05-28 18:41:17 -1200

    blog con base de datos

--- BLOG.php
+++ BLOG.php
@@ -10,14 +10,24 @@
 			</div>
 		</section>
         <main class="main-content">
-            <section class="container blogs">
                 <br>
 				<h1 class="heading-1">Últimos Blogs</h1>
+<!-- inicio php -->
 
-				<div class="container-blogs">
+<section class="container blogs">
+<div class="container-blogs">
+
+<?php 
+	require 'includes/config/database.php';
+	$db=conectarDB();
+    $con_sql="SELECT * FROM blog";
+    $res=mysqli_query($db,$con_sql);
+    while($reg=$res->fetch_assoc())
+    {
+?>
 					<div class="card-blog">
 						<div class="container-img">
-							<img src="img/blog-1.jpg" alt="Imagen Blog 1" />
+							<img src="admin/blog/imagenes/<?php echo $reg['imagen']; ?>"  alt="Imagen Blog 2" />
 							<div class="button-group-blog">
 								<span>
 									<i class="fa-solid fa-magnifying-glass"></i>
@@ -28,305 +38,25 @@
 							</div>
 						</div>
 						<div class="content-blog">
-							<h3>Entre Libros y Letras</h3>
+							<h3><?php echo $reg['titulo']; ?></h3>
 							<span>31 octubre 2023</span>
 							<p>
-								Un espacio dedicado a reseñas de libros de 
-								diversos géneros, todos aquellos que 
-							    encuentran refugio, inspiración y compañía 
-								entre las páginas de un libro así como 
-								recomendaciones de lecturas y reflexiones 
-								sobre el mundo literario.
+								<?php echo $reg['contenido']; ?>
 							</p>
 							<div class="btn-read-more">Leer más</div>
 						</div>
 					</div>
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog-2.jpg" alt="Imagen Blog 2" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Viaje Literario</h3>
-							<span>14 julio 2023</span>
-							<p>
-								Un espacio donde los libros se convierten 
-								en mapas cómo la geografía y las palabras
-								en senderos que nos aventuraremos por rincones
-								remotos llevando a través de emocionantes 
-								viajes y reseñas de libros literarios por 
-								todo el mundo.
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog-3.jpg" alt="Imagen Blog 3" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>El Rincón del Escritor</h3>
-							<span>03 mayo 2023</span>
-							<p>
-								Un espacio acogedor donde los amantes de las 
-								letras pueden encontrar inspiración, recursos, 
-								consejos para escritores, técnicas 
-								de escritura, libros sobre 
-								escritura, entrevistas con autores reconocidos
-								y apoyo para dar vida a sus historias.
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-				</div>
-                <div class="container-blogs">
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog1.jpg" alt="Imagen Blog 1" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Como conseguir Amigos en Literatura?</h3>
-							<span>01 agosto 2023</span>
-							<p>
-                                A veces nos sentimos solitarios, y la única
-                                compañia que tenemos son nuestro libros que 
-                                nos llevan a imaginar una realidad íncreible
-                                donde no estamos en soledad pero aquí estamos
-                                nosotros para mostrar que podemos acompañarte
-                                a crear una realidad en compañia. 
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog2.jpeg" alt="Imagen Blog 2" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Metodo de Circulo de Lectura (WIKKA)</h3>
-							<span>13 enero 2023</span>
-							<p>
-                                Con este método aprenderas a convocar las 
-                                buenas brivas, a motivarte a lo que te propongas
-                                a cumplir tus metas o sueños.
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog3.jpeg" alt="Imagen Blog 3" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Bloqueo del Escritor o Lector</h3>
-							<span>07 junio 2023</span>
-							<p>
-                                La mayoría del tiempo, como grandes lectores
-                                o escritores nos sentimos atorados como si
-                                no pudieramos despegar nuestras alas a eso 
-                                queridos lectores se llama bloqueo de lector o 
-                                escritor......
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-				</div>
-                <div class="container-blogs">
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog4.jpeg" alt="Imagen Blog 1" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Como mantenerse Concentrado y no Morir en el Intento</h3>
-							<span>12 abril 2023</span>
-							<p>
-                                Ya nos conocemos y yo los comprendo ya se que 
-                                es posible de mantenerse concentrado pero ya 
-                                llego su Mesias! tu salvador papichulo vengo
-                                a ti a enseñarlos las tecnicas prohibidas o 
-                                secretas ocultas por Rosa Negra......
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog5.jpeg" alt="Imagen Blog 2" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>5 Pasos para realizar tu Tesis</h3>
-							<span>24 diciembre 2023</span>
-							<p>
-                                En visperas de navidad su santa literaria
-                                y les trae tremendo regalo para que se acuerden
-                                que sus papás no les ayudan o regalan sus Tesis
-                                se hacen los dificiles, no lloren aqui estamos
-                                para ayudarlos a realizar sus Tesis.
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog6.jpeg" alt="Imagen Blog 3" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Como Evitar las Tentaciones</h3>
-							<span>12 febrero 2023</span>
-							<p>
-								Ya estamos aquí por quienes lloraban a nosotros
-                                los únicos inigualables los FBI (Feos Bonitos Inalcansables)
-                                recordandoles que este 14 de Febrero tienes que
-                                olvidar el celular o telefono......
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-				</div>
-                <div class="container-blogs">
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog7.jpeg" alt="Imagen Blog 1" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Los Pasos de Lectura</h3>
-							<span>27 marzo 2023</span>
-							<p>
-                                Ustedes saben que los lectores siguen
-                                unos pasos muy especificos para una gran
-                                comodidad y lectura.
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog8.jpeg" alt="Imagen Blog 2" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Como crear un Club de Lectura</h3>
-							<span>29 septiembre 2023</span>
-							<p>
-								1. Reunir a la gente o amigos(as)
-                                2. Buscar un Tema a Debatir y Leer
-                                3. Tomar atención 
-                                4. Escuchar atentamente
-                                5. Preguntas o Debate de Lectura
-                                6. ......
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-					<div class="card-blog">
-						<div class="container-img">
-							<img src="img/blog9.jpg" alt="Imagen Blog 3" />
-							<div class="button-group-blog">
-								<span>
-									<i class="fa-solid fa-magnifying-glass"></i>
-								</span>
-								<span>
-									<i class="fa-solid fa-link"></i>
-								</span>
-							</div>
-						</div>
-						<div class="content-blog">
-							<h3>Como no aburrirse con tu Lectura</h3>
-							<span>10 noviembre 2023</span>
-							<p>
-								A veces los lectores nos fatigamos por el
-                                trama, los personajes, el objetivo, el creador
-                                de la obra, ect. 
-                                Pero para eso les traigo estos consejos
-                                el primero y el más valioso ......
-							</p>
-							<div class="btn-read-more">Leer más</div>
-						</div>
-					</div>
-				</div>
+			
+<?php 
+	}
+?>
+</div>
+<!-- fin de php -->
 			</section>
-		</main>
-		<?php 
-		include "../espaciodeliteratura/includes/templates/footer.php"; 
-		?>
-		<script
-			src="https://kit.fontawesome.com/81581fb069.js"
-		></script>
+</main>
+<?php 
+include "../espaciodeliteratura/includes/templates/footer.php"; 
+?>
+<script
+	src="https://kit.fontawesome.com/81581fb069.js"
+></script>
