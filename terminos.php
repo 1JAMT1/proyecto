3a0bb04cd030d967d6e0c53fa06de2a735163a94
JaureguiCoraiteAlexisEstaban <153701015+JaureguiCoraiteAlexisEstabn@users.noreply.github.com>
2024-05-29 10:46:29 -0400

    Update terminos.php

--- terminos.php
+++ terminos.php
@@ -1,35 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Terminos y Condiciones</title>
-    <link rel="stylesheet" href="cssc/bootstrap.min.css" />
-    <link rel="stylesheet" href="cssc/font-awesome.min.css" />
-    <link rel="stylesheet" href="cssc/contact.css" />
+<?php
+    $inicio=false;
+    include "../espaciodeliteratura/includes/templates/header.php";
+?>
+    <link rel="stylesheet" href="../../cssa/bootstrap.min.css">
+    <link rel="stylesheet" href="../../cssc/font-awesome.min.css" />
+    <link rel="stylesheet" href="../../cssc/contact.css" />
+    <link rel="stylesheet" href="estilo.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,700,900,900i">
-</head>
-<body>
-    <center><h1 class="titulo-contacto text-center">TERMINOS Y CONDICIONES</h1></center>
-    <h3>1. Propiedad Intelectual:</h3>
-    <p>- Todo el contenido publicado en este espacio, incluyendo pero no limitado a textos, imágenes, y cualquier otra forma de expresión artística, es propiedad intelectual de sus respectivos autores.</p>
-    <p>- Al publicar contenido en este espacio, otorgas una licencia no exclusiva a la administración del espacio para mostrar, distribuir y promocionar tu obra dentro del marco de este espacio de literatura.</p>
-    <h3>2. Responsabilidad del Contenido:</h3>
-    <p>- Los autores son los únicos responsables del contenido que publican en este espacio. La administración no se hace responsable por el contenido generado por los usuarios.</p>
-    <p>- No se permite la publicación de material que sea ilegal, difamatorio, obsceno, ofensivo, o que viole los derechos de propiedad intelectual de terceros.</p>
-    <h3>3. Comportamiento del Usuario:</h3>
-    <p>- Los usuarios deben comportarse de manera respetuosa y cortés hacia otros miembros de la comunidad.</p>
-    <p>- No se tolerará el acoso, la intimidación, o cualquier forma de comportamiento abusivo hacia otros usuarios.</p>
-    <h3>4. Privacidad:</h3>
-    <p>- La administración se compromete a proteger la privacidad de los usuarios y no compartirá su información personal con terceros sin su consentimiento, a menos que así lo requiera la ley.</p>
-    <p>- Los usuarios son responsables de proteger su propia privacidad y no deben compartir información personal sensible en este espacio.</p>
-    <h3>5. Modificación de Términos y Condiciones:</h3>
-    <p>- La administración se reserva el derecho de modificar estos términos y condiciones en cualquier momento sin previo aviso.</p>
-    <p>- Los cambios entrarán en vigor inmediatamente después de su publicación en este espacio.</p>
     <br>
-    <p>Última actualización: 26/05/2024</p>
-    <script src="jsc/jquery-1.12.4.min.js"></script>
-    <script src="jsc/bootstrap.min.js"></script>
-    <script src="jsc/custom.js"></script>
-</body>
-</html>
+    <center><h1 class="titulo">TERMINOS Y CONDICIONES</h1></center>
+    <h2>1. Propiedad Intelectual:</h2>
+    <h5>- Todo el contenido publicado en este espacio, incluyendo pero no limitado a textos, imágenes, y cualquier otra forma de expresión artística, es propiedad intelectual de sus respectivos autores.</h5>
+    <h5>- Al publicar contenido en este espacio, otorgas una licencia no exclusiva a la administración del espacio para mostrar, distribuir y promocionar tu obra dentro del marco de este espacio de literatura.</h5>
+    <h2>2. Responsabilidad del Contenido:</h2>
+    <h5>- Los autores son los únicos responsables del contenido que publican en este espacio. La administración no se hace responsable por el contenido generado por los usuarios.</h5>
+    <h5>- No se permite la publicación de material que sea ilegal, difamatorio, obsceno, ofensivo, o que viole los derechos de propiedad intelectual de terceros.</h5>
+    <h2>3. Comportamiento del Usuario:</h2>
+    <h5>- Los usuarios deben comportarse de manera respetuosa y cortés hacia otros miembros de la comunidad.</h5>
+    <h5>- No se tolerará el acoso, la intimidación, o cualquier forma de comportamiento abusivo hacia otros usuarios.</h5>
+    <h2>4. Privacidad:</h2>
+    <h5>- La administración se compromete a proteger la privacidad de los usuarios y no compartirá su información personal con terceros sin su consentimiento, a menos que así lo requiera la ley.</h5>
+    <h5>- Los usuarios son responsables de proteger su propia privacidad y no deben compartir información personal sensible en este espacio.</h5>
+    <h2>5. Modificación de Términos y Condiciones:</h2>
+    <h5>- La administración se reserva el derecho de modificar estos términos y condiciones en cualquier momento sin previo aviso.</h5>
+    <h5>- Los cambios entrarán en vigor inmediatamente después de su publicación en este espacio.</h5>
+    <br>
+    <h5>Última actualización: 26/05/2024</h5>
+    <br>
+<?php 
+	include "../espaciodeliteratura/includes/templates/footer.php"; 
+?>
+    <script src="../../jsa/bootstrap.min.js" ></script>
+<script
+	src="https://kit.fontawesome.com/81581fb069.js"
+></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
