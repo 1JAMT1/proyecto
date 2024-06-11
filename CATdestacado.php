3219457248bbcdf6b713bc93eb888a6106266855
PortugalCU <celesteusnayo@gmail.com>
2024-06-11 13:15:10 -0400

    CATdestacado.php

--- CATdestacado.php
+++ CATdestacado.php
@@ -50,12 +50,12 @@
   </div>
 </div>
 
-<h1 class="heading-1">Libros góticos</h1>
+<h1 class="heading-1">Libros de terror</h1>
 <div class="container-products">
 
                 <?php
                     $x=1;
-                    $consql = "SELECT * FROM libros where generoLibro='Gótico' ORDER BY RAND()";
+                    $consql = "SELECT * FROM libros where generoLibro='Terror' ORDER BY RAND()";
                     $resultado=mysqli_query($db,$consql);
                     while($var=mysqli_fetch_array($resultado))
                     {
@@ -147,6 +147,203 @@
                 ?>
 </div>
         <br><br>
+
+<h1 class="heading-1">Libros de fantasia</h1>
+<div class="container-products">
+
+                <?php
+                    $x=1;
+                    $consql = "SELECT * FROM libros where generoLibro='Fantasia' ORDER BY RAND()";
+                    $resultado=mysqli_query($db,$consql);
+                    while($var=mysqli_fetch_array($resultado))
+                    {
+                ?>
+
+<div class="card-product">
+						<div class="container-img">
+							<img src="/espaciodeliteratura/admin/libros/imagenes/<?php echo $var['portada']; ?>" alt="Cafe Irish" />
+							<div class="button-group">
+								<span>
+									<i class="fa-regular fa-eye"></i>
+								</span>
+								<span>
+									<i class="fa-regular fa-heart"></i>
+								</span>
+								<span>
+									<i class="fa-solid fa-code-compare"></i>
+								</span>
+							</div>
+						</div>
+                        <div class="content-card-product">
+							<div class="stars">
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-regular fa-star"></i>
+							</div>
+							<h3><?php echo $var['titulo']; ?></h3>
+              <span class="add-cart">
+			<a href="/espaciodeliteratura/admin/pedido/registrarpedido1.php?idu=<?php echo $id;?>&
+			idl=<?php echo $var['idlibro']; ?>"><i class="fa-solid fa-basket-shopping"></i></a>
+			</span>
+							<p class="price"><?php echo $var['precio']; ?></p>
+						</div>
+					</div>
+                <?php
+                    $x = $x+1;
+                    }
+                ?>
+</div>
+        <br><br>
+
+	    <h1 class="heading-1">Libros de romancer</h1>
+<div class="container-products">
+
+                <?php
+                    $x=1;
+                    $consql = "SELECT * FROM libros where generoLibro='Romance' ORDER BY RAND()";
+                    $resultado=mysqli_query($db,$consql);
+                    while($var=mysqli_fetch_array($resultado))
+                    {
+                ?>
+
+<div class="card-product">
+						<div class="container-img">
+							<img src="/espaciodeliteratura/admin/libros/imagenes/<?php echo $var['portada']; ?>" alt="Cafe Irish" />
+							<div class="button-group">
+								<span>
+									<i class="fa-regular fa-eye"></i>
+								</span>
+								<span>
+									<i class="fa-regular fa-heart"></i>
+								</span>
+								<span>
+									<i class="fa-solid fa-code-compare"></i>
+								</span>
+							</div>
+						</div>
+                        <div class="content-card-product">
+							<div class="stars">
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-regular fa-star"></i>
+							</div>
+							<h3><?php echo $var['titulo']; ?></h3>
+              <span class="add-cart">
+			<a href="/espaciodeliteratura/admin/pedido/registrarpedido1.php?idu=<?php echo $id;?>&
+			idl=<?php echo $var['idlibro']; ?>"><i class="fa-solid fa-basket-shopping"></i></a>
+			</span>
+							<p class="price"><?php echo $var['precio']; ?></p>
+						</div>
+					</div>
+                <?php
+                    $x = $x+1;
+                    }
+                ?>
+</div>
+        <br><br>
+
+<h1 class="heading-1">Libros de poesia</h1>
+<div class="container-products">
+
+                <?php
+                    $x=1;
+                    $consql = "SELECT * FROM libros where generoLibro='Poesia' ORDER BY RAND()";
+                    $resultado=mysqli_query($db,$consql);
+                    while($var=mysqli_fetch_array($resultado))
+                    {
+                ?>
+
+<div class="card-product">
+						<div class="container-img">
+							<img src="/espaciodeliteratura/admin/libros/imagenes/<?php echo $var['portada']; ?>" alt="Cafe Irish" />
+							<div class="button-group">
+								<span>
+									<i class="fa-regular fa-eye"></i>
+								</span>
+								<span>
+									<i class="fa-regular fa-heart"></i>
+								</span>
+								<span>
+									<i class="fa-solid fa-code-compare"></i>
+								</span>
+							</div>
+						</div>
+                        <div class="content-card-product">
+							<div class="stars">
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-regular fa-star"></i>
+							</div>
+							<h3><?php echo $var['titulo']; ?></h3>
+              <span class="add-cart">
+			<a href="/espaciodeliteratura/admin/pedido/registrarpedido1.php?idu=<?php echo $id;?>&
+			idl=<?php echo $var['idlibro']; ?>"><i class="fa-solid fa-basket-shopping"></i></a>
+			</span>
+							<p class="price"><?php echo $var['precio']; ?></p>
+						</div>
+					</div>
+                <?php
+                    $x = $x+1;
+                    }
+                ?>
+</div>
+        <br><br>
+
+<h1 class="heading-1">Libros de cocina</h1>
+<div class="container-products">
+
+                <?php
+                    $x=1;
+                    $consql = "SELECT * FROM libros where generoLibro='Cocina' ORDER BY RAND()";
+                    $resultado=mysqli_query($db,$consql);
+                    while($var=mysqli_fetch_array($resultado))
+                    {
+                ?>
+
+<div class="card-product">
+						<div class="container-img">
+							<img src="/espaciodeliteratura/admin/libros/imagenes/<?php echo $var['portada']; ?>" alt="Cafe Irish" />
+							<div class="button-group">
+								<span>
+									<i class="fa-regular fa-eye"></i>
+								</span>
+								<span>
+									<i class="fa-regular fa-heart"></i>
+								</span>
+								<span>
+									<i class="fa-solid fa-code-compare"></i>
+								</span>
+							</div>
+						</div>
+                        <div class="content-card-product">
+							<div class="stars">
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-solid fa-star"></i>
+								<i class="fa-regular fa-star"></i>
+							</div>
+							<h3><?php echo $var['titulo']; ?></h3>
+              <span class="add-cart">
+			<a href="/espaciodeliteratura/admin/pedido/registrarpedido1.php?idu=<?php echo $id;?>&
+			idl=<?php echo $var['idlibro']; ?>"><i class="fa-solid fa-basket-shopping"></i></a>
+			</span>
+							<p class="price"><?php echo $var['precio']; ?></p>
+						</div>
+					</div>
+                <?php
+                    $x = $x+1;
+                    }
+                ?>
+</div>
+        <br><br>
+	    
     </main>
     <script src="../../jsa/bootstrap.min.js" ></script>
 <?php
